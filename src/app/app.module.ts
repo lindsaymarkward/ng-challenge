e2619c82e5b10301f1228ab58a010ea3499788ae
Lindsay Ward <lindsay.ward@jcu.edu.au>
2016-08-23 22:48:16 +1000

    Add AngularFire2 and get basic database list working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule, ApplicationRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBP3IERkXI8lyO8_vACVZd4MAwM9kTZjsU",
+  authDomain: "firedemo-4ee41.firebaseapp.com",
+  databaseURL: "https://firedemo-4ee41.firebaseio.com",
+  storageBucket: "firedemo-4ee41.appspot.com",
+};
 
 @NgModule({
   declarations: [
@@ -11,7 +19,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CommonModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   entryComponents: [AppComponent],
