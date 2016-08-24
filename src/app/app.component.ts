f509a89067e995f17d5d03ecc16ae79210d92083
Lindsay Ward <lindsay.ward@jcu.edu.au>
2016-08-24 19:58:29 +1000

    Add sorting by score

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import {Component} from '@angular/core';
+import {AngularFire} from 'angularfire2';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -8,10 +9,16 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 })
 export class AppComponent {
   title = 'app works!';
-  users: FirebaseListObservable<any[]>;
+  users: Observable<any[]>;  // note: tried using 'FirebaseListObservable<any[]>' but got type conversion error
 
   constructor(af: AngularFire) {
-    this.users = af.database.list('/users');
+    this.users = af.database.list('/users')
+      .map(items => items.sort((a, b) => {
+        if (a.score <= b.score) {
+          return 1;
+        }
+        return -1;
+      }));
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule, environment } from './app/';
+import 'rxjs/add/operator/map';
 
 if (environment.production) {
   enableProdMode();
